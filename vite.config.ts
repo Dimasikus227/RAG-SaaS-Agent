c9ef6d1ee3135fc7bac05bd621e4690b81c77f3f
Dimasikus227 <0660141988a@gmail.com>
2025-05-21 16:40:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/RAG-SaaS-Agent/', // ДОДАЙ ЦЕЙ РЯДОК
   server: {
     host: "::",
     port: 8080,
